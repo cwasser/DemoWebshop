0528be751abfe47c6a19fc5e7aa3e7917233a4f9
cwasser <christian.wasser@trivago.com>
2016-02-11 17:32:10 +0100

    - generated prod.js - generated product placeholder entity - configured database connection (config will not pushed)

---
+++ src/DemoShopBundle/Entity/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DemoShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="DemoShopBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
